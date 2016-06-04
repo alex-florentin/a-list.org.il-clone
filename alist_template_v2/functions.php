21f62f19f0b0a96bd01fc30df766978cea1a4642
alex-florentin <alexflorentinn@gmail.com>
2016-06-04 11:50:45 +0300

    Save 1: working in functions.php (creating manage users page)

--- alist_template_v2/functions.php
+++ alist_template_v2/functions.php
@@ -1,14 +1,12 @@
 <?php
-
 register_nav_menus ( array(
 'main-menu' => 'Main Menu'
 )); // register main menu
-
 add_theme_support( 'post-thumbnails' );
+add_theme_support( 'html5',array('search-form') );
 add_image_size('home-thumbnail', 300, auto, true);
 add_image_size('profile-image', 500, auto, true);
 //register thumbnails
-
 function arphabet_widgets_init() {
 	register_sidebar( array(		'name'          => 'Text on featured panel',
 		'id'            => 'text-featured-panel',
@@ -17,8 +15,6 @@ function arphabet_widgets_init() {
 		'before_title'  => '<div class="text-title">',
 		'after_title'   => '</div>',
 	) );
-
-
 	register_sidebar( array(		'name'          => 'Text on contact page',
 		'id'            => 'text-contact-page',
 		'before_widget' => '',
@@ -26,7 +22,6 @@ function arphabet_widgets_init() {
 		'before_title'  => '<div class="text-title">',
 		'after_title'   => '</div>',
 	) );
-
 	register_sidebar( array(
 		'name'          => 'Email on footer',
 		'id'            => 'email-footer',
@@ -35,7 +30,6 @@ function arphabet_widgets_init() {
 		'before_title'  => '<div class="hidden-title">',
 		'after_title'   => '</div>',
 	) );
-
 	register_sidebar( array(
 		'name'          => 'Phone on footer',
 		'id'            => 'phone-footer',
@@ -44,7 +38,6 @@ function arphabet_widgets_init() {
 		'before_title'  => '<div class="hidden-title">',
 		'after_title'   => '</div>',
 	) );
-
 	register_sidebar( array(
 		'name'          => 'Location on footer',
 		'id'            => 'location-footer',
@@ -53,101 +46,51 @@ function arphabet_widgets_init() {
 		'before_title'  => '<div class="hidden-title">',
 		'after_title'   => '</div>',
 	) );
-
 }
 add_action( 'widgets_init', 'arphabet_widgets_init' );
 // Widgets
-
 add_action( 'admin_menu', 'my_admin_menu' );
-
 function my_admin_menu() {
-	add_menu_page( 'Order profiles page', 'Order profiles', 'manage_options', 'order-profiles-page.php', 'order_profiles_page', 'dashicons-admin-generic', 120  );
+	add_menu_page( 'Manage users page', 'Manage users', 'manage_options', 'manage-users-page.php', 'manage_users_page', 'dashicons-welcome-view-site', 120  );
 }
 
-function order_profiles_page(){
+function manage_users_page(){
+	$output = '';
+	mysqli_connect('localhost', 'alexflor_alist', 'U9B_CxijWz:/X{zdJg', 'alexflor_alist') or die ("Connexion error or dB not found. Check functions.php line 58.");
+	if (isset($_POST['search'])) {
+		$search_query = $_POST['search'];
+		$query = mysqli_query("SELECT * FROM wp_people_table WHERE name LIKE '%$search_query%' OR last_name LIKE '%$search_query%'") or die("Something happened! Could not search!");
+		$count = mysqli_num_rows($query);
+		if($count == 0) {
+			$output = "There was not search results!"
+		} else {
+			while ($row = mysql_fetch_array($query)) {
+				$email = $row['email'];
+				$name = $row['name'];
+				$last_name = $row['last_name'];
+
+				$output = "
+				<p>$email</p>
+				<p>$name</p>
+				<p>$last_name</p>
+				";
+			}
+		}
+	} // if isset
+
 	?>
 
-		<?php
-		$mysqli=new mysqli('localhost', 'agenda_alist2', '#AcBV8,D(0o5TZ3&QZ', 'agenda_alist2');
-		if(mysqli_connect_error()){
-			echo "Connexion failed!", mysqli_connect_error();
-		}
-		$query="Select * FROM wp_people_table";
-		$resultado=$mysqli->query($query);
-		?>
+	<h2>Manage users</h2>
 
-		<h2>Order profiles</h2>
-
-		<div id="message" class="error" style="margin:0;margin-bottom:10px;"><p><b>After finishing the reorganization of, DON'T FORGET to click the 'Delete Cache' button. You can find it in the bar above.</p></div>
-
-		<table class="wp-list-table widefat fixed striped people">
-			<thead>
-				<tr>
-					<th class="manage-column column-name column-primary sortable asc" scope="col">
-						<a href="#">Profile image</a>
-					</th>
-					<th>
-						<a href="#">Email</a>
-					</th>
-					<th>
-						<a href="#">Order number</a>
-					</th>
-					<th>
-						<a href="#">Action</a>
-					</th>
-				</tr>
-			</thead>
-			<tbody id="the-list">
-
-		<?php while($row=$resultado->fetch_assoc()){ ?>
-				<form action="/wp-admin/admin.php?page=order-profiles-page.php" method="POST">
-					<input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-					<tr>
-						<td class="column-name has-row-actions column-primary">
-							<img src="/wp-content/themes/alist_template_v2/uploads/profile/size/<?php echo $row['profile_image'] ?>" style="width:150px;height:auto" />
-						</td>
-						<td class="email column-email">
-							<p>
-								<?php echo $row['email'] ?>
-							</p>
-						</td>
-						<td class="email column-order_number">
-							<input type="text" name="order_number" value="<?php echo $row['order_number'] ?>">
-						</td>
-						<td class="submit column-submit">
-							<input type="submit" name="submit" value="Submit">
-						</td>
-					</tr>
-				</form>
-		<?php } ?>
-			</tbody>
-		</table>
+	<form method="post">
+		<input type="text" name="search" placeholder="email, name & last_name" size="30">
+		<input type="submit" value="Go!">
+	</form>
 
 
-		<?php
-			if (isset($_POST['submit'])) {
+	<?php
 
-				$id=$_POST['id'];
-				$order_number=$_POST['order_number'];
-
-				$query="UPDATE wp_people_table SET order_number='$order_number' WHERE id='$id'";
-
-				$resultado=$mysqli->query($query);
-
-				$sql = "ALTER TABLE `wp_people_table` ORDER BY `order_number`;";
-
-				$sql=$mysqli->query($sql);
-
-				if ($resultado>0) {
-					echo "<script>window.location.reload(true)</script>";
-				}
-
-			}
-		?>
-
-} // order profiles page
-
-<?php
+} // order profiles page function
 
 function truncate($text, $chars = 270) {
     $text = $text." ";
@@ -156,6 +99,4 @@ function truncate($text, $chars = 270) {
     $text = $text."...";
     return $text;
 }
-
-
 ?>
