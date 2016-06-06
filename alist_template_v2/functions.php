b1c231ac7fb3bd33172c1259f8e56aafa64f7c73
alex-florentin <alexflorentinn@gmail.com>
2016-06-06 13:00:56 +0300

    Search using mysqli done

--- alist_template_v2/functions.php
+++ alist_template_v2/functions.php
@@ -55,59 +55,40 @@ function my_admin_menu() {
 }
 
 function manage_users_page(){
-	$output = '';
-	$connection = mysql_connect("localhost", "alexflor_alist", "U9B_CxijWz:/X{zdJg") or die ("Could not connect with dB!");
-	$db_select = mysql_select_db("alexflor_alist") or die ("Could not find dB!");
-	if (isset($_POST['search'])) {
-		$search_query = $_POST['search'];
-		$query = mysql_query("SELECT * FROM wp_people_table WHERE name LIKE '%$search_query%' OR last_name LIKE '%$search_query%' OR email LIKE '%$search_query%'");
-		$count = mysql_num_rows($query);
-		if($count == 0) {
-			$output = "There was not search results!";
+
+	$conn = mysqli_connect ('localhost', 'alexflor_alist', 'U9B_CxijWz:/X{zdJg', 'alexflor_alist') or die ("Connexion problem!");
+
+	$output = "";
+
+	if(isset($_POST['query']) && ($_POST['query'] !== '')) {
+		$search_query = $_POST['query'];
+		$query = mysqli_query ($conn, "SELECT * FROM wp_people_table WHERE name LIKE '%$search_query%' OR last_name LIKE '%$search_query%' OR email LIKE '%$search_query%'") or die (mysqli_error);
+		$count = mysqli_num_rows($query);
+		if ($count == 0) {
+			$output = "No search results for <b>$search_query</b>";
 		} else {
-			while ($row = mysql_fetch_array($query)) {
-				$id = $row['id'];
-				$email = $row['email'];
-				$name = $row['name'];
+			while($row = mysqli_fetch_array($query)) {
+				$first_name = $row['name'];
 				$last_name = $row['last_name'];
-				$what = strip_tags($row->what_drives_me);
-				$public_profile = $row['public_profile'];
-
-
-
-				if($public_profile == "Y") {
-					$checked = "checked";
-				} else {
-					$checked = "";
-				}
-
 				$output .= "
-				<form action='' method='POST'>
-					<input type='hidden' name='id' value='$id'>
-					<input type='checkbox' name='public' value='Y' $checked><span>Public?</span>
-					<input type='submit' name='submit' value='Submit'>
+				<form>
+					<p>$first_name</p>
+					<p>$last_name</p>
 				</form>
 				";
-
-				if(isset($_POST['submit'])) {
-					$w_id = $_POST['id'];
-					$w_public = $_POST['public'];
-					$query="UPDATE wp_people_table SET public_profile='$w_public' WHERE id='$id'";
-				}
-
 			}
 		}
-	} // if isset
+	}
 
 	?>
 
 	<h2>Manage users</h2>
 
 	<form method="post">
-		<input type="text" name="search" size="30">
+		<input type="text" name="query" size="30">
 		<input type="submit" value="Go!">
 	</form>
-
+	<?php echo $output; ?>
 
 	<?php
 
