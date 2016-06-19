f097ee58d8a07951cce2f4fb1688d478893e81c1
alex-florentin <alexflorentinn@gmail.com>
2016-06-19 20:56:09 +0300

    Done

--- alist_template_v2/functions.php
+++ alist_template_v2/functions.php
@@ -52,6 +52,7 @@ add_action( 'widgets_init', 'arphabet_widgets_init' );
 add_action( 'admin_menu', 'my_admin_menu' );
 function my_admin_menu() {
 	add_menu_page( 'Manage users page', 'Manage users', 'manage_options', 'manage-users-page.php', 'manage_users_page', 'dashicons-welcome-view-site', 120  );
+	add_menu_page( 'Order profiles page', 'Order profiles', 'manage_options', 'order-profiles-page.php', 'order_profiles_page', 'dashicons-admin-generic', 120  );
 }
 
 function manage_users_page(){
@@ -327,7 +328,6 @@ function manage_users_page(){
 		$twitter = $_POST['twitter'];
 		$linkedin = $_POST['linkedin'];
 
-
 		$update_query = "UPDATE wp_people_table SET
 		order_number = '$order_number',
 		featured = '$featured_user',
@@ -512,6 +512,95 @@ function manage_users_page(){
 <?php
 
 } // manage_users_page
+
+function order_profiles_page(){
+	?>
+
+		<?php
+
+		$mysqli=new mysqli('localhost', 'agenda_alist2', '#AcBV8,D(0o5TZ3&QZ', 'agenda_alist2');
+
+
+		if(mysqli_connect_error()){
+			echo "Connexion failed!", mysqli_connect_error();
+		}
+
+		$query="Select * FROM wp_people_table";
+		$resultado=$mysqli->query($query);
+		?>
+
+
+
+		<h2>Order profiles</h2>
+
+		<div id="message" class="error" style="margin:0;margin-bottom:10px;"><p><b>After finishing the reorganization of, DON'T FORGET to click the 'Delete Cache' button. You can find it in the bar above.</p></div>
+
+		<table class="wp-list-table widefat fixed striped people">
+			<thead>
+				<tr>
+					<th class="manage-column column-name column-primary sortable asc" scope="col">
+						<a href="#">Profile image</a>
+					</th>
+					<th>
+						<a href="#">Email</a>
+					</th>
+					<th>
+						<a href="#">Order number</a>
+					</th>
+					<th>
+						<a href="#">Action</a>
+					</th>
+				</tr>
+			</thead>
+			<tbody id="the-list">
+		<?php while($row=$resultado->fetch_assoc()){ ?>
+				<form action="/wp-admin/admin.php?page=order-profiles-page.php" method="POST">
+					<input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+					<tr>
+						<td class="column-name has-row-actions column-primary">
+							<img src="/wp-content/themes/alist_template_v2/uploads/profile/size/<?php echo $row['profile_image'] ?>" style="width:150px;height:auto" />
+						</td>
+						<td class="email column-email">
+							<p>
+								<?php echo $row['email'] ?>
+							</p>
+						</td>
+						<td class="email column-order_number">
+							<input type="text" name="order_number" value="<?php echo $row['order_number'] ?>">
+						</td>
+						<td class="submit column-submit">
+							<input type="submit" name="submit" value="Submit">
+						</td>
+					</tr>
+				</form>
+		<?php } ?>
+			</tbody>
+		</table>
+
+
+		<?php
+			if (isset($_POST['submit'])) {
+
+				$id=$_POST['id'];
+				$order_number=$_POST['order_number'];
+
+				$query="UPDATE wp_people_table SET order_number='$order_number' WHERE id='$id'";
+
+				$resultado=$mysqli->query($query);
+
+				$sql = "ALTER TABLE `wp_people_table` ORDER BY `order_number`;";
+
+				$sql=$mysqli->query($sql);
+
+				if ($resultado>0) {
+					echo "<script>window.location.reload(true)</script>";
+				}
+
+			}
+		?>
+
+	<?php
+} // order profiles page
 
 function truncate($text, $chars = 270) {
     $text = $text." ";
